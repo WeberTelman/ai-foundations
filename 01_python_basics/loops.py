7a5b5231ea0b6c7ac227b81797d33665efff7b04
WeberTelman <97873561+WeberTelman@users.noreply.github.com>
2026-01-11 20:37:01 +0400

    Create loops.py

---
+++ 01_python_basics/loops.py
@@ -0,0 +1,12 @@
+# loops.py
+
+print("Counting from 1 to 10")
+
+for i in range(1, 11):
+    print(i)
+
+print("\nEven numbers from 1 to 20")
+
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
