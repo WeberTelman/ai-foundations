ebb3f7eea8d3af20f88a77e1c83a0877bdd734e2
WeberTelman <97873561+WeberTelman@users.noreply.github.com>
2026-01-11 20:37:49 +0400

    Create functions.py

---
+++ 01_python_basics/functions.py
@@ -0,0 +1,10 @@
+# functions.py
+
+def greet(name):
+    return f"Hello, {name}! Welcome to AI."
+
+def square(number):
+    return number * number
+
+print(greet("Telman"))
+print("5 squared is", square(5))
